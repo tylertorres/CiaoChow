287a51593bdc6236845e8acfe6dbe9090a450e6e
tylertorres <tylertorresp@gmail.com>
2024-10-07 21:32:55 -0400

    Initial Commit

---
+++ CiaoChow/CiaoChowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiaoChowApp.swift
+//  CiaoChow
+//
+//  Created by Tyler Torres on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CiaoChowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
